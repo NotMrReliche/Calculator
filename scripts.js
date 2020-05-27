d90887564b9ef52a0f5803f15cc949e624ec4491
NotMrReliche <william.staffordcoyte@protonmail.com>
2020-05-27 15:44:37 -0400

    basic calculate finsihed 051720

--- scripts.js
+++ scripts.js
@@ -12,16 +12,20 @@ keys.addEventListener("click",e => {
         const action = key.dataset.action
         const value = key.textContent
         const displayNumber = display.textContent
+        const previousKeyType = calculator.dataset.previousKeyType
 
         if (!action){
-            if (displayNumber==="0"){
+            if (displayNumber==="0"||previousKeyType==="operator"){
+                calculator.dataset.previousKeyType="666";
                 display.textContent = value
             } else{
                 display.textContent = displayNumber+value
             }
         }
         if (action==="add" ||action==="subtract" ||action==="multiply" ||action==="divide"){
-            
+            calculator.dataset.previousKeyType="operator";
+            calculator.dataset.firstValue=displayNumber
+            calculator.dataset.operator=action
           
         }
         if (action==="decimal"){
@@ -31,7 +35,24 @@ keys.addEventListener("click",e => {
             display.textContent = "0"
         }    
         if (action==="equal"){
-            console.log ("calculate")
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayNumber
+            display.textContent=calculate(firstValue,operator,secondValue)
+            calculator.dataset.previousKeyType="operator";
         }
     }
 })
+const calculate = (n1,operator,n2)=>{
+    let result = "";
+    if (operator==="add"){
+        result = parseFloat(n1)+parseFloat(n2)
+    } else if (operator==="subtract"){
+        result = parseFloat(n1)-parseFloat(n2)
+    }else if (operator==="multiply"){
+        result = parseFloat(n1)*parseFloat(n2)
+    }else if (operator==="divide"){
+        result = parseFloat(n1)/parseFloat(n2)
+    }
+    return result
+}
